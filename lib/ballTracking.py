6db07ba16575ca624c343a74bc445713cc675096
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-21 12:05:54 -0300

    Configurando teste de ballTracking

--- lib/ballTracking.py
+++ lib/ballTracking.py
@@ -5,20 +5,17 @@ import cropImage
 low_threshold=40
 high_threshold=120
 
-def tracking(img):
+def tracking(img, showImages = False):
     response = 404
-    img = cropImage.cropVertical(img)
+    # img = cropImage.cropVertical(img)
 
     blur = cv2.medianBlur(img,5)
     cimg = cv2.cvtColor(blur,cv2.COLOR_GRAY2BGR)
-    # cv2.imshow('gray',cimg)
 
     kernel = np.ones((5,5), np.uint8) 
     morpho = cv2.morphologyEx(cimg, cv2.MORPH_CLOSE, kernel)
-    # cv2.imshow("morphologyEx", morpho)    
 
     edged = cv2.Canny(morpho, low_threshold, high_threshold)
-    # cv2.imshow("edged", edged)    
 
     circles = cv2.HoughCircles(edged,cv2.HOUGH_GRADIENT,1,20,
                                 param1=50,param2=30,minRadius=0,maxRadius=0)
@@ -32,20 +29,34 @@ def tracking(img):
             cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
         
 
-        # cv2.imshow('detected circles',cimg)
 
         response = -255
-    
-    # if cv2.waitKey(15000) & 0xFF == ord('q'):
-    #     cv2.destroyAllWindows()
+
+    if (showImages):
+        cv2.imshow("morphologyEx", morpho)    
+        cv2.imshow("edged", edged)    
+        cv2.imshow('detected circles',cimg)
+
+        if cv2.waitKey(15000) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+
 
     return response
 
 
 def test():
-    img = cv2.imread("/home/felipe/Documentos/LineDetect/RaspLineDetect/image/ball_gray.jpg",0)
+    print("Para o teste ser bem sucedido escolha uma das imagens da pasta /image:")
+    print("ball_gray")
+    print("ball_gray_alone")
+    print("ball_gray_triangle")
+    print("ball_black")
+    print("ball_black_alone")
+    print("ball_black_triangle")
+    fileName = input('Informe a imagem da pasta a ser verificada: ')
 
-    response = tracking(img)
+    img = cv2.imread(f'../image/{fileName}.jpg',0)
+
+    response = tracking(img, True)
 
     if (response == -255):
         print("HAVE BALL")
